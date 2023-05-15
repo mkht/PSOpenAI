7931c631755a3a3f6ad4930b8041fb2542eecf82
mkht <mkht@users.noreply.github.com>
2023-05-15 22:35:50 +0900

    minor performance improvement?

--- Libs/CustomAttributes/src/CompletionsAttribute.cs
+++ Libs/CustomAttributes/src/CompletionsAttribute.cs
@@ -7,7 +7,7 @@ public class CompletionsAttribute : ArgumentCompleterAttribute
     private static ScriptBlock _createScriptBlock(params string[] completions)
     {
         string text = "\"" + string.Join("\",\"", completions) + "\"";
-        string code = "param($Command, $Parameter, $WordToComplete, $CommandAst, $FakeBoundParams);@(" + text + ") -like \"$WordToComplete*\" | Foreach-Object { [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_) }";
+        string code = $"param($Command, $Parameter, $WordToComplete, $CommandAst, $FakeBoundParams);@({text}) -like \"$WordToComplete*\" | Foreach-Object {{ [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_) }}";
         return ScriptBlock.Create(code);
     }
 
