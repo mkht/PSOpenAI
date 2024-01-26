0832460fb69b7b32ca46bd2c83794f812ea5cc09
mkht <mkht@users.noreply.github.com>
2024-01-26 19:43:52 +0900

    Add new model names to tab completions for Request-ChatCompletion

--- Public/Request-ChatCompletion.ps1
+++ Public/Request-ChatCompletion.ps1
@@ -25,10 +25,13 @@ function Request-ChatCompletion {
             'gpt-3.5-turbo-0613',
             'gpt-3.5-turbo-16k-0613',
             'gpt-3.5-turbo-1106',
+            'gpt-3.5-turbo-0125',
             'gpt-4-0613',
             'gpt-4-32k',
             'gpt-4-32k-0613',
+            'gpt-4-turbo-preview',
             'gpt-4-1106-preview',
+            'gpt-4-0125-preview',
             'gpt-4-vision-preview'
         )]
         [string][LowerCaseTransformation()]$Model = 'gpt-3.5-turbo',
