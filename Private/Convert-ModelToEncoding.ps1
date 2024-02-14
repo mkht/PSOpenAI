9f33de746730377b92e118cd053e8b049820c3f6
mkht <mkht@users.noreply.github.com>
2024-02-15 00:21:16 +0900

    Add new models to encoding mapper

--- Private/Convert-ModelToEncoding.ps1
+++ Private/Convert-ModelToEncoding.ps1
@@ -11,8 +11,18 @@ function Convert-ModelToEncoding {
         'gpt-3.5-turbo-*' { 'cl100k_base'; continue } # e.g, gpt-3.5-turbo-16k, -0401, etc.
         'gpt-4-*' { 'cl100k_base'; continue }  # e.g., gpt-4-0613, gpt-4-32k, etc.
         'gpt-3.5-turbo' { 'cl100k_base'; continue }
+        'gpt-3.5' { 'cl100k_base'; continue }  # Common shorthand
+        'gpt-35-turbo' { 'cl100k_base'; continue }  # Azure deployment name
         'gpt-4' { 'cl100k_base'; continue }
-        # text
+        # base
+        'davince-002' { 'cl100k_base'; continue }
+        'babbage-002' { 'cl100k_base'; continue }
+        # embeddings
+        'text-embedding-ada-002' { 'cl100k_base' ; continue }
+        'text-embedding-3-small' { 'cl100k_base' ; continue }
+        'text-embedding-3-large' { 'cl100k_base' ; continue }
+        # DEPRECATED MODELS
+        # text (DEPRECATED)
         'text-davinci-003' { 'p50k_base'; continue }
         'text-davinci-002' { 'p50k_base'; continue }
         'text-davinci-001' { 'r50k_base'; continue }
@@ -23,18 +33,17 @@ function Convert-ModelToEncoding {
         'curie' { 'r50k_base'; continue }
         'babbage' { 'r50k_base' ; continue }
         'ada' { 'r50k_base' ; continue }
-        # codex
+        # codex (DEPRECATED)
         'code-davinci-002' { 'p50k_base' ; continue }
         'code-davinci-001' { 'p50k_base' ; continue }
         'code-cushman-002' { 'p50k_base' ; continue }
         'code-cushman-001' { 'p50k_base' ; continue }
         'davinci-codex' { 'p50k_base' ; continue }
         'cushman-codex' { 'p50k_base' ; continue }
-        # edit
+        # edit (DEPRECATED)
         'text-davinci-edit-001' { 'p50k_edit' ; continue }
         'code-davinci-edit-001' { 'p50k_edit' ; continue }
-        # embeddings
-        'text-embedding-ada-002' { 'cl100k_base' ; continue }
+        # embeddings (DEPRECATED)
         'text-similarity-davinci-001' { 'r50k_base' ; continue }
         'text-similarity-curie-001' { 'r50k_base' ; continue }
         'text-similarity-babbage-001' { 'r50k_base' ; continue }
