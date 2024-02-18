30bbf1f4b6bdc803064785ed522f9aa67d00ecb9
mkht <mkht@users.noreply.github.com>
2024-02-18 19:03:52 +0900

    Add new models for tab completion

--- Public/Runs/Start-ThreadRun.ps1
+++ Public/Runs/Start-ThreadRun.ps1
@@ -29,10 +29,13 @@ function Start-ThreadRun {
             'gpt-3.5-turbo-0613',
             'gpt-3.5-turbo-16k-0613',
             'gpt-3.5-turbo-1106',
+            'gpt-3.5-turbo-0125',
             'gpt-4-0613',
             'gpt-4-32k',
             'gpt-4-32k-0613',
+            'gpt-4-turbo-preview',
             'gpt-4-1106-preview',
+            'gpt-4-0125-preview',
             'gpt-4-vision-preview'
         )]
         [string][LowerCaseTransformation()]$Model = 'gpt-3.5-turbo',
