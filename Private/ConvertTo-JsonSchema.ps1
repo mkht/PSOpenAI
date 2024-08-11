f662204038ca260ed73ffcdbee5279cc253a8464
mkht <mkht@users.noreply.github.com>
2024-08-11 23:56:20 +0900

    Add some tricks to improve the response accuracy of the model.

--- Private/ConvertTo-JsonSchema.ps1
+++ Private/ConvertTo-JsonSchema.ps1
@@ -25,6 +25,32 @@ function _TreatJsonSchemaObjectProprs {
 
         if ($RemoveUnsupportedKeyWords) {
             foreach ($p in $props) {
+                # Use the trick to improve the response accuracy of the model by giving a description instead of keywords.
+                if ($SchemaObj.properties.$p.'format' -eq 'date-time') {
+                    $SchemaObj.properties.$p | Add-Member -MemberType NoteProperty -Name 'description' -Value 'Parsable date-time format string. For example, "2018-11-13T08:20:39"' -Force
+                }
+                elseif ($SchemaObj.properties.$p.'format' -eq 'date') {
+                    $SchemaObj.properties.$p | Add-Member -MemberType NoteProperty -Name 'description' -Value 'It represents a date in the following format: "YYYY-MM-DD"' -Force
+                }
+                elseif ($SchemaObj.properties.$p.'format' -eq 'time') {
+                    $SchemaObj.properties.$p | Add-Member -MemberType NoteProperty -Name 'description' -Value 'It represents a time in the following format: "hh:mm:ss.s"' -Force
+                }
+                elseif ($SchemaObj.properties.$p.'format' -eq 'regex') {
+                    $SchemaObj.properties.$p | Add-Member -MemberType NoteProperty -Name 'description' -Value 'This property should be a valid regular expression string.' -Force
+                }
+                elseif ($SchemaObj.properties.$p.'format' -eq 'email') {
+                    $SchemaObj.properties.$p | Add-Member -MemberType NoteProperty -Name 'description' -Value 'This property should be a valid e-mail address format.' -Force
+                }
+                elseif ($SchemaObj.properties.$p.'format' -eq 'uri') {
+                    $SchemaObj.properties.$p | Add-Member -MemberType NoteProperty -Name 'description' -Value 'This property should be a valid URI' -Force
+                }
+                elseif ($SchemaObj.properties.$p.'format' -eq 'ipv4') {
+                    $SchemaObj.properties.$p | Add-Member -MemberType NoteProperty -Name 'description' -Value 'It represents a IPv4 address.' -Force
+                }
+                elseif ($SchemaObj.properties.$p.'format' -eq 'ipv6') {
+                    $SchemaObj.properties.$p | Add-Member -MemberType NoteProperty -Name 'description' -Value 'It represents a IPv6 address.' -Force
+                }
+
                 $UnsupportedKeyWords.ForEach({
                         $SchemaObj.properties.$p.PSObject.Properties.Remove($_)
                     })
