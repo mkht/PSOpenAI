1deaad0ba6a1d5ddab13ca8e344e3f58991e856a
mkht <mkht@users.noreply.github.com>
2024-10-13 03:53:26 +0900

    Add test for realtime

---
+++ Tests/Realtime/Realtime.E2E.tests.ps1
@@ -0,0 +1,127 @@
+#Requires -Modules @{ ModuleName="Pester"; ModuleVersion="5.3.0" }
+
+BeforeAll {
+    $script:ModuleRoot = Split-Path (Split-Path $PSScriptRoot -Parent) -Parent
+    $script:ModuleName = 'PSOpenAI'
+    $script:TestData = Join-Path (Split-Path $PSScriptRoot -Parent) 'TestData'
+    Import-Module (Join-Path $script:ModuleRoot "$script:ModuleName.psd1") -Force
+}
+
+Describe 'Realtime E2E Test' {
+    Context 'End-to-End tests (online)' -Tag 'Online' {
+        BeforeAll {
+            Clear-OpenAIContext
+            $script:ReceiveEventSource = 'PSOpenAI.Realtime.ReceiveMessage'
+            $script:SendEventSource = 'PSOpenAI.Realtime.SendMessage'
+        }
+
+        Context 'Text In / Text Out' {
+            BeforeAll {
+                Disconnect-OpenAIRealtimeSession
+
+                $global:ReceiveStack = [System.Collections.Concurrent.ConcurrentStack[object]]::new()
+                $global:SendStack = [System.Collections.Concurrent.ConcurrentStack[string]]::new()
+
+                # Register event handler
+                $null = Register-EngineEvent -SourceIdentifier $script:ReceiveEventSource -Action {
+                    $global:ReceiveStack.Push($Event.SourceArgs[0])
+                }
+                $null = Register-EngineEvent -SourceIdentifier $script:SendEventSource -Action {
+                    $global:SendStack.Push($Event.SourceArgs[0])
+                }
+
+                $script:Instructions = 'You are a helpful assistant.'
+                $script:PromptMessage = 'Name one of the most famous people in Spain.'
+                $script:PromptNextMessage = 'What about Germany?'
+            }
+
+            AfterAll {
+                $global:ReceiveStack.Clear()
+                $global:SendStack.Clear()
+                Unregister-Event -SourceIdentifier $script:ReceiveEventSource
+                Unregister-Event -SourceIdentifier $script:SendEventSource
+                Get-Job | Remove-Job -Force
+            }
+
+            It 'STEP1: Connect to session' {
+                { Connect-OpenAIRealtimeSession -Model 'gpt-4o-realtime-preview-2024-10-01' -ea Stop } | Should -Not -Throw
+            }
+
+            It 'STEP1-1: Only one session is allowed' {
+                { Connect-OpenAIRealtimeSession -Model 'gpt-4o-realtime-preview-2024-10-01' -ea Stop } | Should -Throw
+            }
+
+            It 'STEP2: Configure session' {
+                $item = $null
+                { Set-OpenAIRealtimeSessionConfiguration `
+                        -Instructions $script:Instructions `
+                        -Modalities 'text' `
+                        -Temperature 0.6 `
+                        -MaxResponseOutputTokens 32 `
+                        -ea Stop } | Should -Not -Throw
+                Start-Sleep -Seconds 1
+
+                $null = $global:SendStack.TryPeek([ref]$item)
+                $item = $item | ConvertFrom-Json
+                $item.type | Should -BeExactly 'session.update'
+                $item.session.instructions | Should -BeExactly $script:Instructions
+
+                $null = $global:ReceiveStack.TryPeek([ref]$item)
+                $item.type | Should -BeExactly 'session.updated'
+                $item.session.instructions | Should -BeExactly $script:Instructions
+            }
+
+            It 'STEP3: Input a user message' {
+                $item = $null
+                { Add-OpenAIRealtimeSessionCoversationItem -Role 'user' -Message $script:PromptMessage -ea Stop } | Should -Not -Throw
+                Start-Sleep -Seconds 1
+
+                $null = $global:SendStack.TryPeek([ref]$item)
+                $item = $item | ConvertFrom-Json
+                $item.type | Should -BeExactly 'conversation.item.create'
+
+                $null = $global:ReceiveStack.TryPeek([ref]$item)
+                $item.type | Should -BeExactly 'conversation.item.created'
+            }
+
+            It 'STEP4: Trigger response' {
+                $item = $null
+                { Request-OpenAIRealtimeSessionResponse -MaxOutputTokens 12 -ea Stop } | Should -Not -Throw
+                Start-Sleep -Seconds 5
+
+                $null = $global:SendStack.TryPeek([ref]$item)
+                $item = $item | ConvertFrom-Json
+                $item.type | Should -BeExactly 'response.create'
+
+                $null = $global:ReceiveStack.TryPeek([ref]$item)
+                $item.type | Should -BeExactly 'response.done'
+                $item.response.output.role | Should -Be 'assistant'
+
+                $items = $global:ReceiveStack.ToArray() | select -ExpandProperty type
+                $items | Should -Not -Contain 'error'
+                $items | Should -Contain 'response.content_part.added'
+                $items | Should -Contain 'response.text.delta'
+                $items | Should -Contain 'response.content_part.done'
+                $items | Should -Contain 'response.output_item.done'
+            }
+
+            It 'STEP5: Input a next message (with trigger response)' {
+                $item = $null
+                { Add-OpenAIRealtimeSessionCoversationItem -Role 'user' -Message $script:PromptNextMessage -TriggerResponse -ea Stop } | Should -Not -Throw
+                Start-Sleep -Seconds 12
+
+                $null = $global:SendStack.TryPeek([ref]$item)
+                $item = $item | ConvertFrom-Json
+                $item.type | Should -BeExactly 'response.create'
+
+                $null = $global:ReceiveStack.TryPeek([ref]$item)
+                $item.type | Should -BeExactly 'response.done'
+                $item.response.output.role | Should -Be 'assistant'
+            }
+
+            It 'STEP6: Close session' {
+                { Disconnect-OpenAIRealtimeSession -ea Stop } | Should -Not -Throw
+            }
+        }
+    }
+}
